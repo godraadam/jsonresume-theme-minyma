86b24b6571bc4ec1d1d548aeafbc79e57e4ab892
godraadam <godra.adam@gmail.com>
2024-03-27 17:43:49 +0200

    Reflect JSON resume schema changes in handlebars template

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const handlebars = require("handlebars");
 const { template, styles } = require("./output.js");
 
+handlebars.registerHelper("notEmpty", (value) => !!value && value.length > 0);
+
 function render(input) {
   const _template = handlebars.compile(template);
   return _template({ ...input, styles });
